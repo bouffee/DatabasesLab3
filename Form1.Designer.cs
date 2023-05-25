2eaa2daeb453e3a8d4666333f36e2ba811222c9f
Alisa <91307985+bouffee@users.noreply.github.com>
2023-05-25 19:56:11 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DatabaseApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button selectRandomButton;
+        private System.Windows.Forms.Button selectRangeButton;
+        private System.Windows.Forms.Button removeAllButton;
+        private System.Windows.Forms.Button insertMillionButton; // New button
+        private System.Windows.Forms.Label connectionStatusLabel;
+        private System.Windows.Forms.ProgressBar progressBar; // Add progressBar control
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.insertButton = new System.Windows.Forms.Button();
+            this.selectRandomButton = new System.Windows.Forms.Button();
+            this.selectRangeButton = new System.Windows.Forms.Button();
+            this.removeAllButton = new System.Windows.Forms.Button();
+            this.insertMillionButton = new System.Windows.Forms.Button();
+            this.connectionStatusLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(10, 10);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(129, 26);
+            this.insertButton.TabIndex = 0;
+            this.insertButton.Text = "Insert Records";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // selectRandomButton
+            // 
+            this.selectRandomButton.Location = new System.Drawing.Point(10, 42);
+            this.selectRandomButton.Name = "selectRandomButton";
+            this.selectRandomButton.Size = new System.Drawing.Size(129, 26);
+            this.selectRandomButton.TabIndex = 1;
+            this.selectRandomButton.Text = "Select Random Records";
+            this.selectRandomButton.UseVisualStyleBackColor = true;
+            this.selectRandomButton.Click += new System.EventHandler(this.selectRandomButton_Click);
+            // 
+            // selectRangeButton
+            // 
+            this.selectRangeButton.Location = new System.Drawing.Point(10, 73);
+            this.selectRangeButton.Name = "selectRangeButton";
+            this.selectRangeButton.Size = new System.Drawing.Size(129, 26);
+            this.selectRangeButton.TabIndex = 2;
+            this.selectRangeButton.Text = "Select Records in Range";
+            this.selectRangeButton.UseVisualStyleBackColor = true;
+            this.selectRangeButton.Click += new System.EventHandler(this.selectRangeButton_Click);
+            // 
+            // removeAllButton
+            // 
+            this.removeAllButton.Location = new System.Drawing.Point(10, 104);
+            this.removeAllButton.Name = "removeAllButton";
+            this.removeAllButton.Size = new System.Drawing.Size(129, 26);
+            this.removeAllButton.TabIndex = 3;
+            this.removeAllButton.Text = "Remove All Data";
+            this.removeAllButton.UseVisualStyleBackColor = true;
+            this.removeAllButton.Click += new System.EventHandler(this.removeAllButton_Click);
+            // 
+            // insertMillionButton
+            // 
+            this.insertMillionButton.Location = new System.Drawing.Point(10, 135);
+            this.insertMillionButton.Name = "insertMillionButton";
+            this.insertMillionButton.Size = new System.Drawing.Size(129, 26);
+            this.insertMillionButton.TabIndex = 4;
+            this.insertMillionButton.Text = "Insert 1000000 values";
+            this.insertMillionButton.UseVisualStyleBackColor = true;
+            this.insertMillionButton.Click += new System.EventHandler(this.insertMillionButton_Click);
+            // 
+            // connectionStatusLabel
+            // 
+            this.connectionStatusLabel.AutoSize = true;
+            this.connectionStatusLabel.Location = new System.Drawing.Point(10, 185);
+            this.connectionStatusLabel.Name = "connectionStatusLabel";
+            this.connectionStatusLabel.Size = new System.Drawing.Size(0, 14);
+            this.connectionStatusLabel.TabIndex = 5;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(10, 166);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(129, 17);
+            this.progressBar.TabIndex = 5;
+            this.progressBar.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 211);
+            this.Controls.Add(this.connectionStatusLabel);
+            this.Controls.Add(this.selectRangeButton);
+            this.Controls.Add(this.selectRandomButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.removeAllButton);
+            this.Controls.Add(this.insertMillionButton);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database App";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Npgsql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DatabaseApp
+{
+    public partial class MainForm : Form
+    {
+        private string connectionString = "Server=localhost;Port=5432;Database=db;User Id=root;Password=root;";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Check the connection
+            if (IsServerConnected(connectionString))
+            {
+                connectionStatusLabel.Text = "Connection successful!";
+            }
+            else
+            {
+                connectionStatusLabel.Text = "Connection failed!";
+            }
+        }
+
+
+        private bool IsServerConnected(string connectionString)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("INSERT INTO Table_1 (Column_1, Column_2, Column_3) VALUES (@Column_1, @Column_2, @Column_3);", connection))
+                {
+                    Random random = new Random();
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        command.Parameters.AddWithValue("@Column_1", i + 1);
+                        command.Parameters.AddWithValue("@Column_2", random.Next(1, 4));
+                        command.Parameters.AddWithValue("@Column_3", "Some random text");
+                        command.ExecuteNonQuery();
+
+                        command.Parameters.Clear();
+                    }
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Time taken to insert 1000 records: " + stopwatch.Elapsed);
+                }
+            }
+        }
+
+        private void selectRandomButton_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 LIMIT 1000;", connection))
+                {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Do something with the retrieved data
+                        }
+                    }
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Time taken to select 1000 random records: " + stopwatch.Elapsed);
+                }
+            }
+        }
+
+        private void selectRangeButton_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 WHERE Column_1 BETWEEN 500000 AND 500999;", connection))
+                {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Do something with the retrieved data
+                        }
+                    }
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Time taken to select 1000 records within a range: " + stopwatch.Elapsed);
+                }
+            }
+        }
+
+        private void removeAllButton_Click(object sender, EventArgs e)
+        {
+            // Remove all records from the table
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("DELETE FROM Table_1;", connection))
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+                    MessageBox.Show($"{rowsAffected} records removed from the table.");
+                }
+            }
+        }
+
+        private void insertMillionButton_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("INSERT INTO Table_1 (Column_1, Column_2, Column_3) VALUES (@Column_1, @Column_2, @Column_3);", connection))
+                {
+                    Random random = new Random();
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    int totalRecords = 350000;
+                    int batchSize = 1000;
+                    int progress = 0;
+
+                    progressBar.Visible = true;
+                    progressBar.Minimum = 0;
+                    progressBar.Maximum = totalRecords;
+                    progressBar.Value = 0;
+
+                    for (int i = 0; i < totalRecords; i += batchSize)
+                    {
+                        progressBar.Value = i;
+
+                        for (int j = 0; j < batchSize; j++)
+                        {
+                            int recordIndex = i + j;
+                            if (recordIndex >= totalRecords)
+                                break;
+
+                            command.Parameters.AddWithValue("@Column_1", recordIndex + 1);
+                            command.Parameters.AddWithValue("@Column_2", random.Next(1, 4));
+                            command.Parameters.AddWithValue("@Column_3", "Some random text");
+                            command.ExecuteNonQuery();
+
+                            command.Parameters.Clear();
+                        }
+
+                        progress = i + batchSize;
+                    }
+
+                    progressBar.Value = totalRecords;
+                    progressBar.Visible = false;
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Time taken to insert 1 million records: " + stopwatch.Elapsed);
+                }
+            }
+        }
+
+    }
+}
