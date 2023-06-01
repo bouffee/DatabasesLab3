09a22281fbd3e8b30d08ff6300abe9116fa1a247
Alisa <91307985+bouffee@users.noreply.github.com>
2023-06-01 10:58:06 +0300

    Make new version of programm

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Windows.Forms;
 using Npgsql;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace DatabaseApp
 {
@@ -20,14 +19,13 @@ namespace DatabaseApp
             // Check the connection
             if (IsServerConnected(connectionString))
             {
-                connectionStatusLabel.Text = "Connection successful!";
+                connectionStatusLabel.Text = "Соединение есть";
             }
             else
             {
-                connectionStatusLabel.Text = "Connection failed!";
+                connectionStatusLabel.Text = "Соединения нет";
             }
         }
-
 
         private bool IsServerConnected(string connectionString)
         {
@@ -45,41 +43,70 @@ namespace DatabaseApp
             }
         }
 
-        private void insertButton_Click(object sender, EventArgs e)
+        private void CreateBtreeIndex()
         {
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
 
-                using (NpgsqlCommand command = new NpgsqlCommand("INSERT INTO Table_1 (Column_1, Column_2, Column_3) VALUES (@Column_1, @Column_2, @Column_3);", connection))
+                using (NpgsqlCommand createIndexCommand = new NpgsqlCommand("CREATE INDEX btree_idx ON Table_1 USING btree (column_1)", connection))
+                {
+                    createIndexCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void CreateHashIndex()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand createIndexCommand = new NpgsqlCommand("CREATE INDEX hash_idx ON Table_1 USING hash (column_1)", connection))
+                {
+                    createIndexCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void InsertData()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("INSERT INTO Table_1 (column_1, column_2, column_3) VALUES (@column_1, @column_2, @column_3);", connection))
                 {
                     Random random = new Random();
                     Stopwatch stopwatch = Stopwatch.StartNew();
 
                     for (int i = 0; i < 1000; i++)
                     {
-                        command.Parameters.AddWithValue("@Column_1", i + 1);
-                        command.Parameters.AddWithValue("@Column_2", random.Next(1, 4));
-                        command.Parameters.AddWithValue("@Column_3", "Some random text");
+                        command.Parameters.AddWithValue("@column_1", i + 1);
+                        command.Parameters.AddWithValue("@column_2", random.Next(1, 4));
+                        command.Parameters.AddWithValue("@column_3", "Some random text");
                         command.ExecuteNonQuery();
 
                         command.Parameters.Clear();
                     }
 
                     stopwatch.Stop();
-                    MessageBox.Show("Time taken to insert 1000 records: " + stopwatch.Elapsed);
+                    MessageBox.Show("Время на вставку 1000 записей: " + stopwatch.Elapsed);
                 }
             }
         }
 
-        private void selectRandomButton_Click(object sender, EventArgs e)
+        private void SelectWithBtreeIndex()
         {
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
 
-                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 LIMIT 1000;", connection))
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 WHERE column_1 = @value;", connection))
                 {
+                    command.Parameters.AddWithValue("@value", 500);
+                    command.CommandTimeout = 0;
+
                     Stopwatch stopwatch = Stopwatch.StartNew();
 
                     using (NpgsqlDataReader reader = command.ExecuteReader())
@@ -91,19 +118,22 @@ namespace DatabaseApp
                     }
 
                     stopwatch.Stop();
-                    MessageBox.Show("Time taken to select 1000 random records: " + stopwatch.Elapsed);
+                    MessageBox.Show("Время на SELECT с B-tree индексом: " + stopwatch.Elapsed);
                 }
             }
         }
 
-        private void selectRangeButton_Click(object sender, EventArgs e)
+        private void SelectWithHashIndex()
         {
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
 
-                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 WHERE Column_1 BETWEEN 500000 AND 500999;", connection))
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 WHERE column_1 = @value;", connection))
                 {
+                    command.Parameters.AddWithValue("@value", 500);
+                    command.CommandTimeout = 0;
+
                     Stopwatch stopwatch = Stopwatch.StartNew();
 
                     using (NpgsqlDataReader reader = command.ExecuteReader())
@@ -115,22 +145,63 @@ namespace DatabaseApp
                     }
 
                     stopwatch.Stop();
-                    MessageBox.Show("Time taken to select 1000 records within a range: " + stopwatch.Elapsed);
+                    MessageBox.Show("Время на SELECT с хэш-индексом: " + stopwatch.Elapsed);
                 }
             }
         }
 
-        private void removeAllButton_Click(object sender, EventArgs e)
+        private void SelectWithRangeBtreeIndex()
         {
-            // Remove all records from the table
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
 
-                using (NpgsqlCommand command = new NpgsqlCommand("DELETE FROM Table_1;", connection))
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 WHERE column_1 BETWEEN @minValue AND @maxValue;", connection))
                 {
-                    int rowsAffected = command.ExecuteNonQuery();
-                    MessageBox.Show($"{rowsAffected} records removed from the table.");
+                    command.Parameters.AddWithValue("@minValue", 100);
+                    command.Parameters.AddWithValue("@maxValue", 200);
+                    command.CommandTimeout = 0;
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Do something with the retrieved data
+                        }
+                    }
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Время на SELECT с B-tree индексом и диапазоном: " + stopwatch.Elapsed);
+                }
+            }
+        }
+
+        private void SelectWithRangeHashIndex()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM Table_1 WHERE column_1 BETWEEN @minValue AND @maxValue;", connection))
+                {
+                    command.Parameters.AddWithValue("@minValue", 100);
+                    command.Parameters.AddWithValue("@maxValue", 200);
+                    command.CommandTimeout = 0;
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Do something with the retrieved data
+                        }
+                    }
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Время на SELECT с хэш-индексом и диапазоном: " + stopwatch.Elapsed);
                 }
             }
         }
@@ -180,10 +251,94 @@ namespace DatabaseApp
                     progressBar.Visible = false;
 
                     stopwatch.Stop();
-                    MessageBox.Show("Time taken to insert 1 million records: " + stopwatch.Elapsed);
+                    MessageBox.Show("Время на вставку 350000 записей: " + stopwatch.Elapsed);
                 }
             }
         }
 
+        private void removeAllButton_Click(object sender, EventArgs e)
+        {
+            // Remove all records from the table
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("DELETE FROM Table_1;", connection))
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+                    MessageBox.Show($"{rowsAffected} записей были удалены.");
+                }
+            }
+        }
+
+        private void compareQueriesButton_Click(object sender, EventArgs e)
+        {
+            // Создаем Б-дерево индекс
+            CreateBtreeIndex();
+
+            // Делаем селект
+            SelectWithBtreeIndex();
+
+            // Делаем селект с диапазоном
+            SelectWithRangeBtreeIndex();
+
+            // Дропаем индекс
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand dropIndexCommand = new NpgsqlCommand("DROP INDEX btree_idx;", connection))
+                {
+                    dropIndexCommand.ExecuteNonQuery();
+                }
+            }
+
+            // Создаем хэш индекс
+            CreateHashIndex();
+
+            // Делаем селект
+            SelectWithHashIndex();
+
+            // Делаем селект с диапазоном
+            SelectWithRangeHashIndex();
+
+            // Дропаем индекс
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand dropIndexCommand = new NpgsqlCommand("DROP INDEX hash_idx;", connection))
+                {
+                    dropIndexCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("INSERT INTO Table_1 (Column_1, Column_2, Column_3) VALUES (@Column_1, @Column_2, @Column_3);", connection))
+                {
+                    Random random = new Random();
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        command.Parameters.AddWithValue("@Column_1", i + 1);
+                        command.Parameters.AddWithValue("@Column_2", random.Next(1, 4));
+                        command.Parameters.AddWithValue("@Column_3", "Some random text");
+                        command.ExecuteNonQuery();
+
+                        command.Parameters.Clear();
+                    }
+
+                    stopwatch.Stop();
+                    MessageBox.Show("Время на вставку 1000 записей: " + stopwatch.Elapsed);
+                }
+            }
+        }
     }
 }
